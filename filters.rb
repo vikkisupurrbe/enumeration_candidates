692891367b26f6d2beefcec9d6aaf6cdc2db5205
vikkisupurrbe <yingjialiu1216@gmail.com>
2025-04-12 21:45:51 -0700

    Implement filters logic

--- filters.rb
+++ filters.rb
@@ -1,16 +1,29 @@
 # In this file we define the methods to help filter out candidates
 # This way, we keep these methods separated from other potential parts of the program
 
+def experienced?(candidate)
+  # Your code Here
+  candidate[:years_of_experience] >= 2
+end
+
 def find(id)
-    # Your code Here
-  end
+  # Your code Here
+  @candidates.find { |candidate| candidate[:id] == id }
+end
   
-  def experienced?(candidate)
-    # Your code Here
-  end
   
-  def qualified_candidates(candidates)
-    # Your code Here
+def qualified_candidates(candidates)
+  # Your code Here
+  candidates.select do |candidate|
+    experienced?(candidate) &&
+    candidate[:github_points] >= 100 &&
+    (candidate[:languages].include?('Ruby') || candidate[:languages].include?('Python')) &&
+    candidate[:date_applied] >= (Date.today - 15) &&
+    candidate[:age] > 17
   end
+end
   
-  # More methods will go below
+# More methods will go below
+def sort_on_experience_and_points(candidates)
+  candidates.sort_by { |c| [-c[:years_of_experience], -c[:github_points]] }
+end
--- main.rb
+++ main.rb
@@ -5,5 +5,17 @@ require './candidates'
 require './filters'
 
 ## Your test code can go here
+puts "Testing experienced?"
+puts experienced?(@candidates[0])
+
+puts "\nTesting find"
+pp find(5)
+pp find(999)
+
+puts "\nTesting qualified_candidates"
+pp qualified_candidates(@candidates)
+
+puts "\nTesting sort_on_experience_and_points"
+pp sort_on_experience_and_points(@candidates)
 
 pp @candidates
